098fe994309ba96a1f0a97c08c4084137f89b992
Stephen Young <StephenJayYoung@gmail.com>
2014-10-01 16:15:22 -0700

    the images are up and at em

--- scripts.js
+++ scripts.js
@@ -1,16 +1,55 @@
 console.log('working');
 
 
+
+
 $(function() {
     $.ajax({
         type: "GET",
         url: "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=b944cc8b49d1fefb91be5009980232c0&format=json&nojsoncallback=1&auth_token=72157647854321898-cf20a8088a540415&api_sig=20332eae5cb39101438ea4d92c4b5323",
         dataType: "json",
         success: function (json) {
-        	console.log(json);
-            // alert(xml.Data[0].City);
-            // result = xml.Code;
-            // document.myform.result1.value = result;
+					// 1. console.log json object (did that, removed it)
+
+					// 2. iterate through the image objects
+					// 2a. find the image objects from json
+					var photos = json.photos.photo;
+					$.each(photos, function(key, photo) {
+						// 3. add images to the body
+						// 3b. figure out real image source
+						var farmID = photo.farm;
+						var serverID = photo.server;
+						var id = photo.id;
+						var secret = photo.secret;
+						var imageURL = 'https://farm' +
+							farmID + '.staticflickr.com/' +
+							serverID + '/' +
+							id + '_' +
+							secret + '.jpg';
+
+						$('<img>') // 3a. create an image
+							.attr('src', imageURL)
+							.attr('alt', 'Image')
+							.appendTo('body'); // 3c. add to body
+					});
+
+					// 4. format images so they look pretty
+					// 4a.
+					// 5. all of the images look pretty!
+
+
+					// $.getJSON( "ajax/test.json", function(images) {
+					// var items = [];
+					// $.each( data, function( key, val ) {
+					// items.push( "<li id='" + key + "'>" + val + "</li>" );
+					// });
+
+					// $( "<ul/>", {
+					// "class": "my-new-list",
+					// html: items.join( "" )
+					// }).appendTo( "body" );
+					// });
+
         },
         error: function(xhr, status, error) {
         	console.log(xhr);
