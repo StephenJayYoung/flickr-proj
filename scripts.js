a306fb72210269bf975b74540ef0f8f2f04b78df
Stephen Young <StephenJayYoung@gmail.com>
2014-11-02 14:14:10 -0800

    cleaned up code a little bit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 console.log('working');
 
-
-
 $.ajax({
 		url: "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=21b8dfca2fb6f130f45f45f53dae714a&per_page=100&format=json",
     type: "GET",
@@ -12,38 +10,36 @@ $.ajax({
 
 success = function (json) {
 
+	var photos = json.photos.photo;
+		$.each(photos), function(key, photo) {
+			var farmID = photo.farm;
+			var serverID = photo.server;
+			var id = photo.id;
+			var secret = photo.secret;
+			var img = document.createElement('img');
+			var imageURL = 'https://farm' +
+				farmID + '.staticflickr.com/' +
+				serverID + '/' +
+				id + '_' +
+				secret + '.jpg';
+			var thisIsIt = $(img).attr({'src': imageUrl,
+	  	'class': 'col-lg-12'});
 
-			var photos = json.photos.photo;
-				$.each(photos), function(key, photo) {
+			 $(thisIsIt).appendTo('body');
 
-				var farmID = photo.farm;
-				var serverID = photo.server;
-				var id = photo.id;
-				var secret = photo.secret;
-				var img = document.createElement('img');
-				var imageURL = 'https://farm' +
-					farmID + '.staticflickr.com/' +
-					serverID + '/' +
-					id + '_' +
-					secret + '.jpg';
-				var thisIsIt = $(img).attr({'src': imageUrl,
-      	'class': 'col-lg-12'});
-
-					 $(thisIsIt).appendTo('body');
-
-				// $.post('<img>') // 3a. create an image
-				// 	.attr('src', imageURL)
-				// 	.attr('alt', 'Image')
-				// 	alert( "Load was performed." )
-			// 		.appendTo('body'); // 3c. add to body
-			// }
-    },
-    error = function(xhr, status, error) {
-    	console.log(xhr);
-    	console.log(status);
-    	console.log(error);
-   }
- }
+		// $.post('<img>') // 3a. create an image
+		// 	.attr('src', imageURL)
+		// 	.attr('alt', 'Image')
+		// 	alert( "Load was performed." )
+	// 		.appendTo('body'); // 3c. add to body
+	// }
+},
+error = function(xhr, status, error) {
+	console.log(xhr);
+	console.log(status);
+	console.log(error);
+}
+}
 
 
 
