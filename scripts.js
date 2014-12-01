022ede13c5daf578b5030e915060c128f80197a6
Stephen Young <StephenJayYoung@gmail.com>
2014-11-30 21:58:37 -0500

    updated athetes page

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 console.log('working');
 
 // promises
-$.ajax({
-    url: "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=21b8dfca2fb6f130f45f45f53dae714a&per_page=100&format=json",
+
+$.ajax("https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=21b8dfca2fb6f130f45f45f53dae714a&per_page=1&format=json", {
     type: "GET",
-    dataType: "json",
+    dataType: "json"
 }).then(function(data, status, xhr) {}, function(xhr, status, error) {});
 
 
-success = function (json) {
+var success = function (json) {
 
 	var photos = json.photos.photo;
 		$.each(photos), function(key, photo) {
@@ -22,10 +22,9 @@ success = function (json) {
 				serverID + '/' +
 				id + '_' +
 				secret + '.jpg';
+
 			var thisIsIt = $(img).attr({'src': imageUrl,
 	  	'class': 'col-lg-12'});
-
-			 $(thisIsIt).appendTo('body');
 
 		$.post('<img>') // 3a. create an image
 			.attr('src', thisIsIt)
